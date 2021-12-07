23b808d672964806dbb5ee5487b8d199c171bf0d
Taeho <mun05170@gmail.com>
2021-12-07 12:11:46 +0900

    칼로리 계산기 완료

--- app.js
+++ app.js
@@ -19,17 +19,20 @@ function getData() {
         const req = JSON.parse(target.response).I2790.row;
 
         // ajax에서 필요한 정보만 json으로 묶어 data로 push
-        req.map((v, i) => {
-          let dt = {};
-          dt.cd = v.FOOD_CD;
-          dt.name = v.DESC_KOR;
-          dt.serving = v.SERVING_SIZE;
-          dt.cal = v.NUTR_CONT1;
-          dt.car = v.NUTR_CONT2;
-          dt.pro = v.NUTR_CONT3;
-          dt.fat = v.NUTR_CONT3;
-          data.push(dt);
-        });
+        if (!req) {
+          alert("검색결과가 없습니다.");
+        } else {
+          req.map((v, i) => {
+            let dt = {};
+            dt.name = v.DESC_KOR;
+            dt.serving = v.SERVING_SIZE;
+            dt.cal = v.NUTR_CONT1;
+            dt.car = v.NUTR_CONT2;
+            dt.pro = v.NUTR_CONT3;
+            dt.fat = v.NUTR_CONT3;
+            data.push(dt);
+          });
+        }
 
         // 도출된 data 기반 HTML 출력 기능
         printData();
@@ -60,14 +63,20 @@ function printData() {
   data.map((v, i) => {
     // 생성할 요소들 정의
     const li = document.createElement("li");
+    li.style.height = "75px";
     const dep1 = document.createElement("div");
     const dep2 = document.createElement("div");
 
-    dep1.setAttribute("id", "dep1");
+    dep1.setAttribute("class", "dep1");
+    dep2.setAttribute("class", "dep2");
+
     // 식품 이름
+    const nameBox = document.createElement("div");
+    nameBox.setAttribute("class", "name_box");
     const foodName = document.createElement("h2");
     foodName.setAttribute("class", "food_name");
     foodName.textContent = v.name;
+    nameBox.appendChild(foodName);
 
     // 1인분
     const span1 = document.createElement("span");
@@ -112,13 +121,16 @@ function printData() {
     //   꺽쇠
     const chev = document.createElement("span");
     chev.setAttribute("class", "chev");
-    chev.textContent = "<";
+    const chevron = document.createElement("i");
+    chevron.setAttribute("class", "fas fa-chevron-left");
+    chev.appendChild(chevron);
 
-    dep1.append(foodName, span1, span2, span3, span4, span5, chev);
+    // dep1 요소 추가
+    dep1.append(nameBox, span1, span2, span3, span4, span5, chev);
 
     li.appendChild(dep1);
 
-    //   dep2
+    /** dep2 */
     // 섭취량 타이틀
     const eatTitle = document.createElement("h4");
     eatTitle.textContent = "섭취량";
@@ -126,61 +138,81 @@ function printData() {
     //   form영역 설정
     const inputForm = document.createElement("form");
     inputForm.setAttribute("id", "inputData");
-    inputForm.setAttribute("data-CD", v.cd);
     const input = document.createElement("input");
     input.setAttribute("type", "number");
     input.setAttribute("value", "1.0");
+    input.setAttribute("step", "0.1");
     const calculBtn = document.createElement("button");
     calculBtn.textContent = "확인";
+
+    // 폼에 input, button 추가
     inputForm.append(input, calculBtn);
+
+    // dep2에 섭취량, 폼 추가
     dep2.append(eatTitle, inputForm);
 
     //  확인 버튼 클릭 시 1인분:입력값 계산하여 출력
     calculBtn.addEventListener("click", (e) => {
       e.preventDefault();
+
+      // 확인 버튼 여러번 클릭 시 기존 ellement 제거
+      const inputReset = document.querySelectorAll(".dep2 .print_data");
+      inputReset.forEach((v, i) => {
+        if (v) {
+          v.remove();
+        }
+      });
+
+      // 확인 버튼 여러번 클릭 시 기존 생성된 저장버튼 중복값 제거
+      const saveBtn = document.querySelector(".save");
+      if (saveBtn) {
+        saveBtn.remove();
+      }
+
       const value = input.value;
       const inputSpan2 = document.createElement("span");
       const inputSpan3 = document.createElement("span");
       const inputSpan4 = document.createElement("span");
       const inputSpan5 = document.createElement("span");
-
-      const inputReset = document.querySelectorAll(".dep2 .print_data");
-      inputReset.forEach((v, i) => {
-        v.innerHTML = "";
-      });
-
-      // 열량
       inputSpan2.setAttribute("class", "print_data");
+      inputSpan3.setAttribute("class", "print_data");
+      inputSpan4.setAttribute("class", "print_data");
+      inputSpan5.setAttribute("class", "print_data");
+
+      /* 1인분:섭취량 소수점 2째 자리에서 반올림 */
+      // 열량
       inputSpan2.textContent = "열량(kcal)";
       const inputCal = document.createElement("span");
       inputCal.textContent = (v.cal * value).toFixed(2);
       inputSpan2.appendChild(inputCal);
 
       //   탄수화물
-      inputSpan3.setAttribute("class", "print_data");
       inputSpan3.textContent = "탄수화물(g)";
       const inputCar = document.createElement("span");
       inputCar.textContent = (v.car * value).toFixed(2);
       inputSpan3.appendChild(inputCar);
 
       //   단백질
-      inputSpan4.setAttribute("class", "print_data");
       inputSpan4.textContent = "단백질(g)";
       const inputPro = document.createElement("span");
       inputPro.textContent = (v.pro * value).toFixed(2);
       inputSpan4.appendChild(inputPro);
 
       //   지방
-      inputSpan5.setAttribute("class", "print_data");
       inputSpan5.textContent = "지방(g)";
       const inputFat = document.createElement("span");
       inputFat.textContent = (v.fat * value).toFixed(2);
       inputSpan5.appendChild(inputFat);
 
+      // 저장버튼 생성
       const save = document.createElement("button");
-      let saveDt = {};
       save.textContent = "저장";
       save.setAttribute("class", "save");
+
+      // 섭취량 계산 값 저장될 JSON
+      let saveDt = {};
+
+      // 저장 버튼 클릭 시 섭취량 데이터를 임시 saveDt에 저장
       save.addEventListener("click", () => {
         saveDt.cal = parseInt(inputCal.textContent);
         saveDt.car = parseInt(inputCar.textContent);
@@ -196,16 +228,32 @@ function printData() {
         addData();
       });
 
+      // dep2에 생성된 요소들 추가
       dep2.append(inputSpan2, inputSpan3, inputSpan4, inputSpan5, save);
     });
 
     li.append(dep1, dep2);
     list.appendChild(li);
   });
+
   // 식품 이름 클릭시 height 조정, chev 회전 이벤트
   document.querySelectorAll(".food_name").forEach((v, i) => {
     v.addEventListener("click", (e) => {
-      console.log(e.currentTarget.parentNode);
+      // 부모요소 li.style 호출
+      let liHeight = e.currentTarget.closest("li").style;
+      // 부모요소 li에서 꺽쇠 호출
+      let chev = e.currentTarget
+        .closest("li")
+        .querySelector(".dep1 .chev .fas");
+
+      // li 높이값 변화하여 dep2 항목 도출
+      if (liHeight.height == "150px") {
+        liHeight.height = "75px";
+        chev.style.transform = "rotate(0deg)";
+      } else {
+        liHeight.height = "150px";
+        chev.style.transform = "rotate(-90deg)";
+      }
     });
   });
 }
@@ -214,7 +262,9 @@ function printData() {
 function addData() {
   // 기능 실행시 기존 숫자 삭제
   document.querySelectorAll("#footer .print_data span").forEach((v, i) => {
-    v.innerHTML = "";
+    if (v) {
+      v.remove();
+    }
   });
   // 총 칼로리
   const totalCal = document.createElement("span");
@@ -254,9 +304,12 @@ document.querySelector("#searchBox").addEventListener("submit", (e) => {
   getData();
 });
 
+// 전체 삭제 버튼 클릭시 이벤트
 document.querySelector("#delete").addEventListener("click", () => {
   totalDt = { cal: 0, car: 0, pro: 0, fat: 0 };
   document.querySelectorAll("#footer .print_data span").forEach((v, i) => {
-    v.innerHTML = "";
+    if (v) {
+      v.remove();
+    }
   });
 });
--- sohyun/app.js
+++ sohyun/app.js
@@ -1,78 +1,81 @@
-document.querySelector('#query').addEventListener('submit', e => {
-    e.preventDefault();
+document.querySelector("#query").addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    console.log(e);
-    const inputData = document.querySelector('#input_id');
-    console.log(inputData.value);
+  console.log(e);
+  const inputData = document.querySelector("#input_id");
+  console.log(inputData.value);
 
-    const xhr = new XMLHttpRequest();
-    const method = "GET";
+  const xhr = new XMLHttpRequest();
+  const method = "GET";
 
-    var url = "http://openapi.foodsafetykorea.go.kr/api/d0ea543ac4184c0ab6fe/I2790/json/1/2/DESC_KOR=";
+  var url =
+    "http://openapi.foodsafetykorea.go.kr/api/d0ea543ac4184c0ab6fe/I2790/json/1/20/DESC_KOR=";
 
+  if (inputData.value != "") {
+    url = url + inputData.value;
+  } else {
+    url =
+      "http://openapi.foodsafetykorea.go.kr/api/d0ea543ac4184c0ab6fe/I2790/json/1/20";
+  }
 
-    if(inputData.value != "") {
-        url = url + inputData.value;
-    } else {
-        url = "http://openapi.foodsafetykorea.go.kr/api/d0ea543ac4184c0ab6fe/I2790/json/1/2";
-    }
+  xhr.onreadystatechange = (e) => {
+    const { target } = e;
+    console.log("tttt22");
 
-    xhr.onreadystatechange = e => {
-        const {target} = e;
-        console.log("tttt22");
-        
-        switch (target.readyState) {
-            case XMLHttpRequest.OPENED:
-                console.log("xhr 초기화");
-                break;
-            case XMLHttpRequest.HEADERS_RECEIVED:
-                console.log("send 호출");
-                break;
-            case XMLHttpRequest.LOADING:
-                console.log("수신중");
-                break;
-            case XMLHttpRequest.DONE:
-                console.log("통신종료");
+    switch (target.readyState) {
+      case XMLHttpRequest.OPENED:
+        console.log("xhr 초기화");
+        break;
+      case XMLHttpRequest.HEADERS_RECEIVED:
+        console.log("send 호출");
+        break;
+      case XMLHttpRequest.LOADING:
+        console.log("수신중");
+        break;
+      case XMLHttpRequest.DONE:
+        console.log("통신종료");
 
-                console.log(target);
+        console.log(target);
 
-                if(target.status == 200) {
-                    console.log("text: %s", target.statusText);
+        if (target.status == 200) {
+          console.log("text: %s", target.statusText);
 
-                    const res = target.response;
+          const res = target.response;
 
-                    console.log(res);
+          console.log(res);
 
-                    var obj = JSON.parse(res);
+          var obj = JSON.parse(res);
 
-                    console.log("obj");
-                    console.log(obj);
-                    console.log(obj.I2790.row[0].DESC_KOR);
+          console.log("obj");
+          console.log(obj);
+          console.log(obj.I2790.row[0].DESC_KOR);
 
-                    
-
-                    document.getElementById("foodName").innerHTML = obj.I2790.row[0].DESC_KOR;
-                    document.getElementById("person1").innerHTML = obj.I2790.row[0].SERVING_SIZE;
-                    document.getElementById("NUTR_CONT1").innerHTML = obj.I2790.row[0].NUTR_CONT1;
-                    document.getElementById("NUTR_CONT2").innerHTML = obj.I2790.row[0].NUTR_CONT2;
-                    document.getElementById("NUTR_CONT3").innerHTML = obj.I2790.row[0].NUTR_CONT3;
-                    document.getElementById("NUTR_CONT4").innerHTML = obj.I2790.row[0].NUTR_CONT4;
-
-
-                } else {
-                    const s = parseInt(target.status / 100);
-                    if(s == 4) {
-                        console.log("400 error");
-                    } else if(s == 5) {
-                        console.log("500 error");
-                    } else {
-                        console.log("error");
-                    }
-                }
-                break;
+          document.getElementById("foodName").innerHTML =
+            obj.I2790.row[0].DESC_KOR;
+          document.getElementById("person1").innerHTML =
+            obj.I2790.row[0].SERVING_SIZE;
+          document.getElementById("NUTR_CONT1").innerHTML =
+            obj.I2790.row[0].NUTR_CONT1;
+          document.getElementById("NUTR_CONT2").innerHTML =
+            obj.I2790.row[0].NUTR_CONT2;
+          document.getElementById("NUTR_CONT3").innerHTML =
+            obj.I2790.row[0].NUTR_CONT3;
+          document.getElementById("NUTR_CONT4").innerHTML =
+            obj.I2790.row[0].NUTR_CONT4;
+        } else {
+          const s = parseInt(target.status / 100);
+          if (s == 4) {
+            console.log("400 error");
+          } else if (s == 5) {
+            console.log("500 error");
+          } else {
+            console.log("error");
+          }
         }
+        break;
     }
+  };
 
-    xhr.open(method, url);
-    xhr.send();
-})
+  xhr.open(method, url);
+  xhr.send();
+});
